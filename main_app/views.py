d3c443c07bc03ed25dd186fdb253154fdeaaa944
bbonning4 <bbonning42@gmail.com>
2023-05-30 02:31:27 -0500

    part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+finches = [
+  {'name': 'Steve', 'species': 'Evening Grosbeak', 'description': 'Yellow', 'age': 2},
+  {'name': 'Carl', 'species': 'Purple Finch', 'description': 'Well, purple', 'age': 3},
+  {'name': 'Queen', 'species': 'Black Rosy-Finch', 'description': 'Kinda grey', 'age': 4},
+]
+
+# Define the home view
+def home(request):
+  # Include an .html file extension - unlike when rendering EJS templates
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
