73c0b2aa89e3c6e33cbdcc3c37a7cd2d895e8576
bbonning4 <bbonning42@gmail.com>
2023-05-31 14:09:00 -0500

    part 3

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('index/', views.index, name='index'),
     path('finch/<int:finch_id>/', views.finch_detail, name='detail'),
+    path('finch/create/', views.FinchCreate.as_view(), name='finch_create'),
+    path('finch/<int:pk>/update/', views.FinchUpdate.as_view(), name='finch_update'),
+    path('finch/<int:pk>/delete/', views.FinchDelete.as_view(), name='finch_delete'),
 ]
